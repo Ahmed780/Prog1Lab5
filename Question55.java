724f5cfcf2d8fe4af840ac75fefffa07fef3da76
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-04-25 02:46:01 -0400

    Update Question55.java

--- Question55.java
+++ Question55.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-// package lab5;
+package lab5;
 
 import java.util.Scanner;
 import java.io.File;
@@ -24,7 +24,7 @@ import java.util.Scanner;
 	
 	public static void main(String[] args) {
 
-    Team teamA = new Team();
+    	Team teamA = new Team();
 	teamA.setName("FCB");
 		
 	Team teamB = new Team();
@@ -33,7 +33,7 @@ import java.util.Scanner;
 	Team teamC = new Team();
 	teamC.setName("Real Madrid");
 
-    System.out.println(teamA.toString());
+    	System.out.println(teamA.toString());
 	System.out.println("Are team A and team B equal? " +teamA.equals(teamB));
 	System.out.println("Are team A and team C equal? " +teamA.equals(teamC));	
 
