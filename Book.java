097ecb502d1ac9694486c82b361f68cf2e99ad6b
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-04-26 03:31:24 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,4 +1,19 @@
-		
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+*This class encapsulates the concept of a book such as title, author, ISBN and year of publishing 
+* @author Uzair Ahmed 
+*/		
 class Book {
     
     private String title;
@@ -7,78 +22,68 @@ class Book {
     private int bookYear;
     private String publisher = "Vanier";
     private static int bookAmount = 0;
-    
-    
+      
     public Book(String title, String ISBN){
-        this.title = title;
-        this.author = author;
-        this.ISBN = ISBN;
-        this.bookYear = bookYear;
-        String publisher;
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.bookYear = bookYear;
+    String publisher;
     }
     
     public String getTitle (){
-        return title;
+    return title;
     }
     
     public String getAuthor(){
-        return author;
+    return author;
     }
     
     public String getISBN(){
-        return ISBN;
+    return ISBN;
     }
     
     public int getBookYear(){
-        return bookYear;
+    return bookYear;
     }
     
     public int getBookAmount(){
-        return bookAmount;
+    return bookAmount;
     }
     
     public String getPublisher(){
-        return publisher;
+    return publisher;
     }
     
     public void setTitle(String newTitle){
-        title = newTitle;
-        
+    title = newTitle;
     }
     
     public void setAuthor(String newAuthor){
-        author = newAuthor;
-        bookAmount++;
-    
+    author = newAuthor;
+    bookAmount++;
     }
     
     public void setISBN(String newISBN){
-        ISBN = newISBN;
-        
+    ISBN = newISBN;    
     }
     
     public void setBookYear(int newBookYear){
-        bookYear = newBookYear;
-        
+    bookYear = newBookYear;    
     }
     
     public void setBookAmount(int newBookAmount){
-        bookAmount = newBookAmount;
-        
+    bookAmount = newBookAmount;    
     }
     
     public void setPublisher(String newPublisher){
-        publisher = newPublisher;
+    publisher = newPublisher;
     }
     
     @Override
     
-    //public String toString(){
-      //  return "\nTitle: " + title + "\nAuthor: " + author + "\nISBN: " + ISBN + "\nPublisher: " + publisher +"\nPublishing year: " 
-        //       + bookYear;
-    //}
-     
-     public String toString(){
+   
+    public String toString(){
         return  "************************************"+
                 "\n*Author:           "+ author     + 
                 "\n*ISBN:             " + ISBN      +
@@ -86,13 +91,14 @@ class Book {
                 "\n*Publisher:        " +publisher  +
                 "\n**********************************"; 
     }
-    
-    
+        
     public boolean equals(Book book2){
-        if (publisher.equals(book2.publisher) && bookAmount == book2.bookAmount){
-            return true;
-        } else {
-            return false;
-        }
+    if (publisher.equals(book2.publisher) && bookAmount == book2.bookAmount){
+    return true;
+    } 
+    else {
+    return false;
     }
+    
+   }
 }
