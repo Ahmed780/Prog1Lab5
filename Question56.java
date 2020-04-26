2b2c23ad6b8fa6723267c593defa70d81385bf51
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-04-25 22:06:52 -0400

    Update Question56.java

--- Question56.java
+++ Question56.java
@@ -1,5 +1,25 @@
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/** 
+ * This  class encapsulates concepts of a team.
+ * @author Uzair Ahmed 
+ */
 public class Tv {
 
+/**
+* @param args the command line arguments
+* @throws java.io.FileNotFoundException
+*/	
 	public static void main(String[] args) { 
  
 	Television tv1 = new Television();
@@ -15,11 +35,9 @@ public class Tv {
 	tv3.setBrand("ROKU");
 	tv3.setPrice(500);
 
-
-     System.out.println(tv1.toString());	
-     System.out.println("Are tv1 and tv2 from the same brand? " + tv1.equals(tv2));
-     System.out.println("Are tv1 and tv3 from the same brand? " + tv1.equals(tv3));
-    
+     	System.out.println(tv1.toString());	
+     	System.out.println("Are tv1 and tv2 from the same brand? " + tv1.equals(tv2));
+     	System.out.println("Are tv1 and tv3 from the same brand? " + tv1.equals(tv3));
 	
  }
 	
