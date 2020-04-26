4f116570289ce9466cf7f03f8ea4545bd578ecbc
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-04-25 22:10:54 -0400

    Update Television.java

--- Television.java
+++ Television.java
@@ -1,3 +1,22 @@
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/** 
+ * This  class encapsulates concepts of a tv including brand and price.
+ assuming a team has only team name as an attribute. Include a constructor,
+ * the accessor and mutator, and methods toString and equals. Write a client
+ * class to test all methods.
+ * @author Uzair Ahmed 
+ */
 public class Television {
   
   private String brand;
